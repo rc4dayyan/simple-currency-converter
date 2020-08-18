ea69f2e252a624d3bf2627b2d6429e4c677ffc87
rc4dayyan <daeytea@gmail.com>
2020-08-18 15:00:42 +0700

    Updates

---
+++ precache-manifest.a60e71978597c707083ea98702824bb8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7daaab2ba68aeae97293618bc5e52b39",
+    "url": "/simple-currency-converter/index.html"
+  },
+  {
+    "revision": "41ef4099aa872133a921",
+    "url": "/simple-currency-converter/static/css/2.755e63a5.chunk.css"
+  },
+  {
+    "revision": "624b51e24ca3ae79c033",
+    "url": "/simple-currency-converter/static/css/main.c3bc11d5.chunk.css"
+  },
+  {
+    "revision": "41ef4099aa872133a921",
+    "url": "/simple-currency-converter/static/js/2.e42a9f69.chunk.js"
+  },
+  {
+    "revision": "7346b4d3185f4c2b69ec842dd41384ad",
+    "url": "/simple-currency-converter/static/js/2.e42a9f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "624b51e24ca3ae79c033",
+    "url": "/simple-currency-converter/static/js/main.a0cb9f8b.chunk.js"
+  },
+  {
+    "revision": "aa3e5a4aa5a271de878c",
+    "url": "/simple-currency-converter/static/js/runtime-main.63f786f0.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/simple-currency-converter/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/simple-currency-converter/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/simple-currency-converter/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/simple-currency-converter/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/simple-currency-converter/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/simple-currency-converter/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/simple-currency-converter/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/simple-currency-converter/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/simple-currency-converter/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/simple-currency-converter/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/simple-currency-converter/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/simple-currency-converter/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/simple-currency-converter/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/simple-currency-converter/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/simple-currency-converter/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/simple-currency-converter/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
